3e4fd8b143bd597217969218095709ff7ac4abf2
Chen Peleg <chenpeleg@gmail.com>
2025-02-16 21:04:32 +0200

    fixed

--- public/content.js
+++ public/content.js
@@ -1,4 +1,21 @@
 function addContent() {
-  const clickMe = 'Click me!';
+    const rows = ['יום הולדת', 'שמח', 'ליאור',];
+
+    for (let i = 0; i < rows.length; i++) {
+        const rowLetters = rows[i];
+        const div = document.getElementById(`row-${i + 1}`);
+        div.innerHTML = [...rowLetters].map((letter, index) => {
+            if (letter === ' ') {
+                return `<span style="white-space: pre"> </span>`
+            }
+            return   `<span id="letter-${index +
+            1}">${letter}</span>`
+        }
+          ).join('');
+
+    }
+
+
 }
+
 addContent();
